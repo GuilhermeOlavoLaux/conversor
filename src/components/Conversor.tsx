7eff10e82a66da1095013398aafe9b67ebc12d90
laux2laux@gmail.com <laux2laux@gmail.com>
2021-12-08 22:35:55 -0300

    adiciona css

--- src/components/Conversor.tsx
+++ src/components/Conversor.tsx
@@ -1,13 +1,12 @@
-import { useState, useEffect } from 'react'
+import { useState, ChangeEvent } from 'react'
 import { Form } from 'react-bootstrap'
 import { valuesApi } from '../api/routes'
 
 export default function Conversor() {
   const [selectedCoin, setSelectedCoin] = useState<string>()
-
+  const [typedValue, setTypedValue] = useState<string>('')
   const [dollarValue, setDollarValue] = useState<string>('')
   const [euroValue, setEuroValue] = useState<string>('')
-
   const [realValue, setRealValue] = useState<string>('')
 
   const now = new Date()
@@ -15,36 +14,78 @@ export default function Conversor() {
   async function setValues(coin: string) {
     if (coin === 'BRL') {
       await valuesApi.get(`/${coin}-USD`).then((res) => {
-        setDollarValue(res.data.BRLUSD.high)
+        const realToDollar = Number(res.data.BRLUSD.high) * Number(typedValue)
+        setDollarValue(String(realToDollar.toFixed(2)))
       })
       await valuesApi.get(`/${coin}-EUR`).then((res) => {
-        setEuroValue(res.data.BRLEUR.high)
+        const realToEuro = Number(res.data.BRLEUR.high) * Number(typedValue)
+        setEuroValue(String(realToEuro.toFixed(2)))
       })
     }
     if (coin === 'USD') {
       await valuesApi.get(`/${coin}-EUR`).then((res) => {
-        setEuroValue(res.data.USDEUR.high)
+        const dollarToEuro = Number(res.data.USDEUR.high) * Number(typedValue)
+        setEuroValue(String(dollarToEuro.toFixed(2)))
       })
       await valuesApi.get(`/${coin}-BRL`).then((res) => {
-        setRealValue(res.data.USDBRL.high)
+        const dollarToReal = Number(res.data.USDBRL.high) * Number(typedValue)
+        setRealValue(String(dollarToReal.toFixed(2)))
       })
     }
 
     if (coin === 'EUR') {
       await valuesApi.get(`/${coin}-USD`).then((res) => {
-        setDollarValue(res.data.EURUSD.high)
+        const euroToDollar = Number(res.data.EURUSD.high) * Number(typedValue)
+        setDollarValue(String(euroToDollar.toFixed(2)))
       })
       await valuesApi.get(`/${coin}-BRL`).then((res) => {
-        setRealValue(res.data.EURBRL.high)
+        const euroToReal = Number(res.data.EURBRL.high) * Number(typedValue)
+        setRealValue(String(euroToReal.toFixed(2)))
       })
     }
   }
 
+  function renderResult(coin: string) {
+    if (coin === 'EUR') {
+      //@ts-ignore
+      document.querySelector('.real').style.display = 'block'
+
+      //@ts-ignore
+      document.querySelector('.dollar').style.display = 'block'
+
+      //@ts-ignore
+      document.querySelector('.euro').style.display = 'none'
+    }
+
+    if (coin === 'USD') {
+      //@ts-ignore
+      document.querySelector('.real').style.display = 'block'
+
+      //@ts-ignore
+      document.querySelector('.dollar').style.display = 'none'
+
+      //@ts-ignore
+      document.querySelector('.euro').style.display = 'block'
+    }
+
+    if (coin === 'BRL') {
+      //@ts-ignore
+      document.querySelector('.real').style.display = 'none'
+
+      //@ts-ignore
+      document.querySelector('.dollar').style.display = 'block'
+
+      //@ts-ignore
+      document.querySelector('.euro').style.display = 'block'
+    }
+  }
+
   function getCoinValue(coin: string) {
-    if (coin === 'undefined') {
+    if (coin === 'undefined' || !typedValue) {
       console.log('erro')
     } else {
       setValues(coin)
+      renderResult(coin)
     }
   }
 
@@ -56,7 +97,10 @@ export default function Conversor() {
 
         <Form.Group controlId='formBasicSelect' className='bootstrap-field'>
           <h5>Valor</h5>
-          <Form.Control type='number' placeholder='Enter email' />
+          <Form.Control
+            type='number'
+            onChange={(e: ChangeEvent<HTMLInputElement>) => setTypedValue(e.target.value)}
+          />
           <h5>Moeda</h5>
           <Form.Control
             as='select'
@@ -79,16 +123,19 @@ export default function Conversor() {
         <h5>Data da consulta</h5>
         <p>Inserir data </p>
 
-        <h5>Moeda selecionada</h5>
-        <p>Inserir moeda</p>
+        <div className='dollar'>
+          <h5>Dolar</h5>
+          <p>{dollarValue}</p>
+        </div>
+        <div className='euro'>
+          <h5>Euro</h5>
+          <p>{euroValue}</p>
+        </div>
 
-        <h5>Real</h5>
-        <p>Inserir real:</p>
-
-        <p>Real {realValue}</p>
-
-        <p>Dolar {dollarValue}</p>
-        <p>Euro {euroValue}</p>
+        <div className='real'>
+          <h5>Real</h5>
+          <p>{realValue}</p>
+        </div>
 
         <hr />
         <p>{now.getFullYear()} - BuscadorCEP! </p>
